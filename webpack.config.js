ed631cec4979b5e3c580822c54f10aa77beb8c54
DESKTOP-K0HS5QI\Dell <vaideska@mail.ru>
2021-03-04 14:35:37 +0300

    add bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,23 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: process.env.NODE_ENV || 'development',
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.js$/,
         exclude: /node_modules/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['css-loader', 'sass-loader'],
       },
     ],
   },
